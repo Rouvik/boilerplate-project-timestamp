808fe2cddfaa568f0df2b3b56f15ae152a09c7a6
Rouvik Maji <majirouvik@gmail.com>
2025-01-17 14:18:49 +0000

    Update and /api fix

--- index.js
+++ index.js
@@ -20,12 +20,17 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
+app.get("/api", function (req, res) {
+  let date = new Date();
+  res.end(`{"unix": ${date.getTime()}, "utc": "${date.toUTCString()}"}`);
+});
+
 app.get("/api/*", function (req, res) {
     let input = req.url.substring(req.url.lastIndexOf('/') + 1);
     let date = isNaN(+input) ? new Date(input) : new Date(+input);
     if(isNaN(date.getTime()))
     {
-      res.end("{ error : \"Invalid Date\" }");
+      res.end("{ \"error\" : \"Invalid Date\" }");
       return;
     }
     res.end(`{"unix": ${date.getTime()}, "utc": "${date.toUTCString()}"}`);
